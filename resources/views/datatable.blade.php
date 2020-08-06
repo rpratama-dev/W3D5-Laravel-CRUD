1f0f6ba58a462828743465967ca260baabf4ae85
rpratama94 <rpratama.sky@gmail.com>
2020-08-06 10:36:01 +0700

    finally project Laravel vs  template Admin LTE

--- resources/views/datatable.blade.php
+++ resources/views/datatable.blade.php
@@ -448,9 +448,7 @@
 
 @push('scripts')
   <script src="{{ asset('/adminlte/plugins/datatables/jquery.dataTables.js') }}"></script>
-  <script src="{{ asset('/adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.js') }}"></script>
-  <script src="{{ asset('/adminlte/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
-  <script src="{{ asset('/adminlte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+  <script src="{{ asset('/adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.js') }}"></script> 
 
   <script>
     $(function () { 
